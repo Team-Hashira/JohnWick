8a729911e649fe8d410d687699ad69aae2fc92eb
Crogen0407 <mymcoding0407@gmail.com>
2025-01-06 02:40:38 +0900

    feat : CrogenAttribute

---
+++ Assets/Crogen/CrogenAttributeExtension/Editor/HideInInspectorByConditionEditor.cs
@@ -0,0 +1,55 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+
+namespace Crogen.AttributeExtension.Editor
+{
+    [CustomPropertyDrawer(typeof(HideInInspectorByCondition))]
+    public class HideInInspectorByConditionEditorEditor : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            HideInInspectorByCondition hideInInspectorByCondition = attribute as HideInInspectorByCondition;
+
+            SerializedProperty boolProperty = property.serializedObject.FindProperty(hideInInspectorByCondition.BooleanPropertyName);
+
+            if (boolProperty == null)
+            {
+                EditorGUI.PropertyField(position, property);
+                return;
+            }
+
+            if(!hideInInspectorByCondition.Reversed)
+            {
+                if(boolProperty.boolValue)
+                    EditorGUI.PropertyField(position, property);
+            }
+            else
+            {
+                if(boolProperty.boolValue == false)
+                    EditorGUI.PropertyField(position, property);
+            }
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            HideInInspectorByCondition hideInInspectorByCondition = attribute as HideInInspectorByCondition;
+            SerializedProperty boolProperty = property.serializedObject.FindProperty(hideInInspectorByCondition.BooleanPropertyName);
+      
+            if(hideInInspectorByCondition.Reversed)
+            {
+                if(boolProperty.boolValue)
+                    return 0f;
+            }
+            else
+            {
+                if(boolProperty.boolValue == false)
+                    return 0f;
+            }
+            
+
+            return EditorGUI.GetPropertyHeight(property);        
+        }
+    }
+}
+#endif
