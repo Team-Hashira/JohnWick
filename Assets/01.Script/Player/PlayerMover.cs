45a2e3348d0c3e352b445717b79aa46912ae6dd7
Crogen0407 <mymcoding0407@gmail.com>
2025-01-06 03:21:22 +0900

    feat : Crouch Jump

--- Assets/01.Script/Player/PlayerMover.cs
+++ Assets/01.Script/Player/PlayerMover.cs
@@ -1,12 +1,25 @@
+using System;
 using Hashira.Entities;
+using UnityEngine;
 
 namespace Hashira.Players
 {
     public class PlayerMover : EntityMover
     {
-        public void UnderJump()
+        Player _player;
+        
+        private void Start()
         {
-            
+            _player = _entity as Player;    
+            //_player.InputReader.            
+        }
+
+        public void UnderJump(bool isUnderJump)
+        {
+            foreach (var platformEffector in FindObjectsByType<PlatformEffector2D>(FindObjectsSortMode.None))
+            {
+                platformEffector.rotationalOffset = isUnderJump ? 180 : 0;
+            }
         }
     }
 }
