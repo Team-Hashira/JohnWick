bd6a511a1cb0c639ded2dff0a8da5a17bdfc1851
Crogen0407 <mymcoding0407@gmail.com>
2025-01-06 18:25:38 +0900

    fix : ShootSpakleEffect

--- Assets/01.Script/SkillSystem/SkillManager.cs
+++ Assets/01.Script/SkillSystem/SkillManager.cs
@@ -1,13 +1,51 @@
 using System;
 using System.Collections.Generic;
+using Crogen.AttributeExtension;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Hashira.SkillSystem
 {
     public class SkillManager : MonoBehaviour
     {
-        public List<Skill> startSkills = new List<Skill>();
-        private static Dictionary<Type, Skill> _currentSkills = new Dictionary<Type, Skill>();
+        [FormerlySerializedAs("startSkills")] public List<string> startSkillNames;
+        private static readonly Dictionary<Type, Skill> _currentSkills = new Dictionary<Type, Skill>();
+
+#if UNITY_EDITOR
+        [Space(25)]
+        [SerializeField] private string _debugSkillName = "new skill";
+        [Button]
+        private void AddSkillForDebug()
+        {
+            AddSkill(Type.GetType(_debugSkillName));
+        }
+#endif
+        private void Awake()
+        {
+            foreach (var startSkillName in startSkillNames)
+            {
+                Type t = Type.GetType(startSkillName);
+                AddSkill(t);
+            }
+        }
+
+
+        public static void AddSkill(Type type) 
+        {
+            var skill = Activator.CreateInstance(type) as Skill;
+            
+            if(skill == null)
+                Debug.LogError($"Could not create skill {type}");
+            
+            if (_currentSkills.TryAdd(type, skill))
+            {
+                Debug.Log($"Add Skill {{{skill.GetType().Name}}}");
+            }
+            else
+            {
+                Debug.Log($"Level Up Skill {{{skill.GetType().Name}}}");
+            }
+        }
         
         public static void AddSkill<T>() where T : Skill, new()
         {
