d7d4e4be2172fcab0d1e09e36fe897121a5180ee
Crogen0407 <mymcoding0407@gmail.com>
2025-01-14 20:13:06 +0900

    fix : Swap event name

--- Assets/01.Script/Player/Player.cs
+++ Assets/01.Script/Player/Player.cs
@@ -46,7 +46,7 @@ namespace Hashira.Players
             InputReader.OnAttackEvent += HandleAttackEvent;
             InputReader.OnMeleeAttackEvent += HandleMeleeAttackEvent;
             InputReader.OnReloadEvent += HandleReloadEvent;
-            InputReader.OnWeaponSawpEvent += HandleWeaponSawpEvent;
+            InputReader.OnWeaponSwapEvent += HandleWeaponSwapEvent;
         }
 
         #region Handles
@@ -77,7 +77,7 @@ namespace Hashira.Players
                 gun.Reload();
         }
 
-        private void HandleWeaponSawpEvent()
+        private void HandleWeaponSwapEvent()
         {
             _weaponHolderCompo.WeaponSawp();
         }
@@ -116,7 +116,7 @@ namespace Hashira.Players
 
             InputReader.OnAttackEvent -= HandleAttackEvent;
             InputReader.OnMeleeAttackEvent -= HandleMeleeAttackEvent;
-            InputReader.OnWeaponSawpEvent -= HandleWeaponSawpEvent;
+            InputReader.OnWeaponSwapEvent -= HandleWeaponSwapEvent;
             InputReader.OnReloadEvent -= HandleReloadEvent;
         }
     }
