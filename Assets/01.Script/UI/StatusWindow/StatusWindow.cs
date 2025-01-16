05698496c247c91cb0490be4fdfa85d48aaff570
Crogen0407 <mymcoding0407@gmail.com>
2025-01-16 20:03:16 +0900

    feat : New Status Panel

--- Assets/01.Script/UI/StatusWindow/StatusWindow.cs
+++ Assets/01.Script/UI/StatusWindow/StatusWindow.cs
@@ -9,7 +9,7 @@ namespace Hashira.UI.StatusWindow
         [SerializeField] private InputReaderSO _inputReaderSO;
         
         //Values
-        private bool _isShowStatusWindow = false;
+        private bool _isShowStatusWindow => gameObject.activeSelf;
         private bool _isFading = false;
         
         private void Awake()
@@ -18,12 +18,12 @@ namespace Hashira.UI.StatusWindow
             _canvasGroup.alpha = 0;
             _inputReaderSO.UIActions.Enable();
             _canvasGroup.interactable = false;
-            _isShowStatusWindow = false;
         }
 
         private void Start()
         {
             _inputReaderSO.OnStatusWindowEnableEvent += HandleStatusWindowEnable;
+            gameObject.SetActive(false);
         }
 
         private void OnDestroy()
@@ -33,34 +33,42 @@ namespace Hashira.UI.StatusWindow
 
         private void HandleStatusWindowEnable()
         {
-            _isShowStatusWindow = !_isShowStatusWindow;
-
+            Debug.Log(nameof(_isFading) + _isFading);
+            Debug.Log(nameof(_isShowStatusWindow) + _isShowStatusWindow);
             //페이드 중일 때는 어떤 것도 콜하지 않음
             if (_isFading == true) return;
             _isFading = true;
             
-            if (_isShowStatusWindow == true) Show();
+            if (_isShowStatusWindow == false) Show();
             else Hide();
         }
 
         ///보여주기
         private void Show()
         {
+            gameObject.SetActive(true);
             _inputReaderSO.PlayerActions.Disable();
-            _inputReaderSO.UIActions.Enable();
             _canvasGroup.DOFade(1, 0.5f)
-                .SetUpdate(true).OnComplete(()=>_isFading = false);
-            _canvasGroup.interactable = true;
+                .SetUpdate(true)
+                .OnComplete(() =>
+                {
+                    _isFading = false;
+                    _canvasGroup.interactable = true;
+                });
         }
         
         ///숨기기
         private void Hide()
         {
             _inputReaderSO.PlayerActions.Enable();
-            _inputReaderSO.UIActions.Disable();
-            _canvasGroup.DOFade(0, 0.5f)
-                .SetUpdate(true).OnComplete(()=>_isFading = false);
             _canvasGroup.interactable = false;
+            _canvasGroup.DOFade(0, 0.5f)
+                .SetUpdate(true)
+                .OnComplete(()=>
+                {
+                    gameObject.SetActive(false);
+                    _isFading = false;
+                });
         }
     }
 }
