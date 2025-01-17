c7c448906ea1fb6a333104c80e6e50207a17085a
dongdong5899 <dong012673@gmail.com>
2025-01-17 14:20:50 +0900

    feat: WeaponParts Class

---
+++ Assets/01.Script/Item/WeaponParts/WeaponParts.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+namespace Hashira.Weapons
+{
+    public class WeaponParts
+    {
+        public WeaponPartsSO WeaponPartsSO;
+
+        public virtual void Equip()
+        {
+
+        }
+        public virtual void UnEquip()
+        {
+
+        }
+    }
+}
