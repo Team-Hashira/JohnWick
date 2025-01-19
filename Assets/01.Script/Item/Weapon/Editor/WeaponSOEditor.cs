995322fbcdb264e90e6e73fa833977e30d3ebb15
Crogen0407 <mymcoding0407@gmail.com>
2025-01-19 13:11:02 +0900

    fix : WeaponSOEditor

--- Assets/01.Script/Item/Weapon/Editor/WeaponSOEditor.cs
+++ Assets/01.Script/Item/Weapon/Editor/WeaponSOEditor.cs
@@ -12,16 +12,17 @@ namespace Hashira.Items.Weapons.Editor
         public static void Draw(WeaponSO weaponSO)
         {
             float inspectorWidth = EditorGUIUtility.currentViewWidth;
+            float mul = inspectorWidth / SlotRect.width;
             float widthOffset = 25f;
             Rect rectResize = new Rect(0, 0, 0, SlotRect.height/SlotRect.width * (inspectorWidth-widthOffset));
             
             GUILayout.Box(new GUIContent(weaponSO.itemSprite.texture), GUI.skin.window, GUILayout.Height(rectResize.height));
             Rect spaceRect = GUILayoutUtility.GetRect(new GUIContent("Preview"), GUI.skin.window, GUILayout.Height(rectResize.height));
-            Vector2 partSlotResize = PartSlotSize * (inspectorWidth/SlotRect.width);
+            Vector2 partSlotResize = PartSlotSize * mul;
             
             foreach (var pair in weaponSO.partsEquipPosDict)
             {
-                Vector2 partSlotReposition = new Vector2(pair.Value.x, -pair.Value.y);
+                Vector2 partSlotReposition = new Vector2(pair.Value.x, -pair.Value.y) * mul;
 
                 partSlotReposition += new Vector2
                 (
@@ -29,7 +30,10 @@ namespace Hashira.Items.Weapons.Editor
                     spaceRect.position.y - inspectorWidth / 2
                 );
 
+                
                 partSlotReposition += new Vector2(-partSlotResize.x/2, partSlotResize.y/2);
+                
+                
                 
                 GUI.color = Color.cyan;
                 {
--- Assets/01.Script/Item/Weapon/Melees/BatWeapon.cs
+++ Assets/01.Script/Item/Weapon/Melees/BatWeapon.cs
@@ -10,6 +10,7 @@ namespace Hashira.Items.Weapons
         {
             base.Attack(damage, isDown);
             Debug.Log("이거 근접 공격하는 고에요");
+            CameraManager.Instance.ShakeCamera(8, 10, 0.15f);
         }
     }
 }
