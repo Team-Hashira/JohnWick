3316b8b6bd5d0b172a9817b68bd74e783d2d9d2b
Crogen0407 <mymcoding0407@gmail.com>
2025-01-19 12:40:10 +0900

    feat : Melee Weapon

---
+++ Assets/01.Script/Item/Weapon/Melees/BatWeapon.cs
@@ -0,0 +1,12 @@
+namespace Hashira.Items.Weapons
+{
+    public class BatWeapon : MeleeWeapon
+    {
+        private int _damage;
+
+        public override void Attack(int damage, bool isDown)
+        {
+            base.Attack(damage, isDown);
+        }
+    }
+}
--- Assets/01.Script/Item/Weapon/Melees/MeleeWeapon.cs
+++ Assets/01.Script/Item/Weapon/Melees/MeleeWeapon.cs
@@ -1,17 +1,27 @@
+using System;
+
 namespace Hashira.Items.Weapons
 {
     public class MeleeWeapon : Weapon
     {
         public MeleeSO MeleeSO { get; private set; }
         
-        public override void WeaponUpdate()
+        public event Action<int> OnAttackEvent;
+        
+        private void HandleDamageSuccessEvent()
         {
-            base.WeaponUpdate();
+            CameraManager.Instance.ShakeCamera(8, 8, 0.2f);
         }
 
         public override void Attack(int damage, bool isDown)
         {
-            base.Attack(damage, isDown);
+            
+        }
+
+        public override object Clone()
+        {
+            MeleeSO = WeaponSO as MeleeSO;
+            return base.Clone();
         }
     }
 }
