afae07dadbe64d3f3ac6d63f4417dbb409abdb59
Crogen0407 <mymcoding0407@gmail.com>
2025-01-20 10:59:54 +0900

    fix : Gun View

--- Assets/01.Script/Item/Weapon/Editor/WeaponSOEditor.cs
+++ Assets/01.Script/Item/Weapon/Editor/WeaponSOEditor.cs
@@ -15,9 +15,22 @@ namespace Hashira.Items.Weapons.Editor
             float mul = inspectorWidth / SlotRect.width;
             float widthOffset = 25f;
             Rect rectResize = new Rect(0, 0, 0, SlotRect.height/SlotRect.width * (inspectorWidth-widthOffset));
-            
-            GUILayout.Box(new GUIContent(weaponSO.itemSprite.texture), GUI.skin.window, GUILayout.Height(rectResize.height));
+
+            var originTexture = weaponSO.itemSprite.texture;
+            Rect spriteRect = weaponSO.itemSprite.rect;
+
+            Texture2D croppedTexture = new Texture2D((int)spriteRect.width, (int)spriteRect.height);
+            croppedTexture.SetPixels(originTexture.GetPixels(
+                (int)spriteRect.x,
+                (int)spriteRect.y,
+                (int)spriteRect.width,
+                (int)spriteRect.height));
+            croppedTexture.Apply();
+            croppedTexture.filterMode = FilterMode.Point;
+            GUILayout.Box(GUIContent.none, GUI.skin.window, GUILayout.Height(rectResize.height));
             Rect spaceRect = GUILayoutUtility.GetRect(new GUIContent("Preview"), GUI.skin.window, GUILayout.Height(rectResize.height));
+            GUI.DrawTexture(new Rect(spaceRect.x, spaceRect.y-spaceRect.height, spaceRect.width, spaceRect.height), croppedTexture, ScaleMode.ScaleToFit);
+            
             Vector2 partSlotResize = PartSlotSize * mul;
             
             foreach (var pair in weaponSO.partsEquipPosDict)
@@ -29,11 +42,8 @@ namespace Hashira.Items.Weapons.Editor
                     inspectorWidth / 2,
                     spaceRect.position.y - inspectorWidth / 2
                 );
-
                 
                 partSlotReposition += new Vector2(-partSlotResize.x/2, partSlotResize.y/2);
-                
-                
                 
                 GUI.color = Color.cyan;
                 {
