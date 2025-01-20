3764865b857bf76d2970bf4075464bdb619affe6
dongdong5899 <dong012673@gmail.com>
2025-01-20 15:21:33 +0900

    feat: _entityWeapon.OnReloadEvent action Event

--- Assets/01.Script/Entity/Component/EntityReloadingIcon.cs
+++ Assets/01.Script/Entity/Component/EntityReloadingIcon.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 namespace Hashira.Entities.Components
 {
-    public class EntityReloadingIcon : MonoBehaviour, IEntityComponent, IAfterInitialzeComponent
+    public class EntityReloadingIcon : MonoBehaviour, IEntityComponent, IAfterInitialzeComponent, IEntityDisposeComponent
     {
         private readonly static int _FillAmountShaderHash = Shader.PropertyToID("_FillAmount");
 
@@ -18,6 +18,12 @@ namespace Hashira.Entities.Components
         public void AfterInit()
         {
             _entityWeapon = _entity.GetEntityComponent<EntityWeapon>();
+            _entityWeapon.OnReloadEvent += HandleReloadEvent;
+        }
+
+        public void Dispose()
+        {
+            _entityWeapon.OnReloadEvent -= HandleReloadEvent;
         }
 
         public void Initialize(Entity entity)
@@ -25,6 +31,7 @@ namespace Hashira.Entities.Components
             _entity = entity;
             _chargingMat = _charging.material;
             _chargingMat.SetFloat(_FillAmountShaderHash, 0);
+            _visualTrm.gameObject.SetActive(false);
         }
 
         private void HandleReloadEvent(float time)
