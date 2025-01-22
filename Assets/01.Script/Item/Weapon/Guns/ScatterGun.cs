cba5b453fb4a55e73ed7a0a254c9f5940b07f45e
dongdong5899 <dong012673@gmail.com>
2025-01-22 23:22:10 +0900

    feat: ScatterGunSO

---
+++ Assets/01.Script/Item/Weapon/Guns/ScatterGun.cs
@@ -0,0 +1,29 @@
+using Crogen.CrogenPooling;
+using UnityEngine;
+
+namespace Hashira.Items.Weapons
+{
+    public class ScatterGun : GunWeapon
+    {
+        public override void Attack(int damage, bool isDown)
+        {
+            base.Attack(damage, isDown);
+            if (isDown == false) return;
+
+            if (Fire() == false) return;
+
+            CameraManager.Instance.ShakeCamera(8, 10, 0.15f);
+        }
+
+        protected override bool Fire()
+        {
+            if (base.Fire() == false) return false;
+
+            Vector3 firePos = _EntityWeapon.VisualTrm.position + _EntityWeapon.transform.rotation * GunSO.firePoint;
+            CreateBullet(firePos);
+            //Effect
+            _EntityWeapon.gameObject.Pop(GunSO.fireSpakleEffect, firePos, Quaternion.LookRotation(Vector3.back, _EntityWeapon.transform.right));
+            return true;
+        }
+    }
+}
