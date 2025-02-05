70c7f95f0310a1bd8659c8c22da046b3f52bb4fc
Crogen0407 <mymcoding0407@gmail.com>
2025-02-05 11:17:25 +0900

    feat : Door Health

--- Assets/01.Script/Object/DoorObject.cs
+++ Assets/01.Script/Object/DoorObject.cs
@@ -1,8 +1,6 @@
 using Crogen.CrogenPooling;
 using Hashira.Entities;
 using System.Collections.Generic;
-using System.Linq;
-using Unity.VisualScripting;
 using UnityEngine;
 
 namespace Hashira.Object
@@ -10,11 +8,30 @@ namespace Hashira.Object
     public class DoorObject : MonoBehaviour, IDamageable
     {
         [SerializeField] private EffectPoolType _breakParticle;
+        [SerializeField] private List<Sprite> _spriteList;
+        private SpriteRenderer _spriteRenderer;
 
-        public EEntityPartType ApplyDamage(int value, RaycastHit2D raycastHit, Transform attackerTrm)
+        [field: SerializeField] public int MaxHealth { get; private set; } = 100;
+        public int CurrentHealth { get; private set; }
+
+		private void Awake()
+		{
+			_spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+			CurrentHealth = MaxHealth;
+		}
+
+		public EEntityPartType ApplyDamage(int value, RaycastHit2D raycastHit, Transform attackerTrm)
         {
-            gameObject.Pop(_breakParticle, transform.position, Quaternion.identity);
-            Destroy(gameObject);
+			CurrentHealth -= value;
+
+            var curPercent = CurrentHealth / (float)MaxHealth;
+            if(_spriteList.Count > 0)
+			    _spriteRenderer.sprite = _spriteList[(int)(curPercent * _spriteList.Count)];
+
+			gameObject.Pop(_breakParticle, transform.position, Quaternion.identity);
+
+            if(CurrentHealth <= 0)
+                Destroy(gameObject);
 
             return EEntityPartType.Body;
         }
