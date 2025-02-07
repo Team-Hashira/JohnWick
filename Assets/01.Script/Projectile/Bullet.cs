00cded45273b83b8d8479e79f2f621b50ead5621
dongdong5899 <dong012673@gmail.com>
2025-02-07 17:03:26 +0900

    feat: Gun Penetration

--- Assets/01.Script/Projectile/Bullet.cs
+++ Assets/01.Script/Projectile/Bullet.cs
@@ -1,5 +1,8 @@
 using Crogen.CrogenPooling;
 using Hashira.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
 using UnityEngine;
 
 namespace Hashira.Projectile
@@ -15,8 +18,12 @@ namespace Hashira.Projectile
         [SerializeField] private EffectPoolType _wallBloodEffect;
         private float _speed;
         private int _damage;
+        private int _penetration;
+        private int _currentPenetration;
         private LayerMask _whatIsTarget;
         private SpriteRenderer _spriteRenderer;
+
+        private List<Collider2D> _penetratedColliderList = new List<Collider2D>();
 
         private void Awake()
         {
@@ -28,17 +35,17 @@ namespace Hashira.Projectile
             if (_isDead) return;
 
             Vector3 movement = transform.right * Time.fixedDeltaTime * _speed;
-            if (_projectileCollider.CheckCollision(_whatIsTarget, out RaycastHit2D[] hits, movement))
+            bool isHit = _projectileCollider.CheckCollision(_whatIsTarget, out RaycastHit2D[] hits, movement);
+            RaycastHit2D hit = hits.ToList().FirstOrDefault(hit => _penetratedColliderList.Contains(hit.collider) == false);
+            if (isHit && hit != default)
             {
-                RaycastHit2D hit = hits[0];
-
                 //Move
                 transform.position += transform.right * hit.distance;
 
                 //Damage
-                int damage = _damage;
                 if (hit.transform.TryGetComponent(out IDamageable damageable))
                 {
+                    int damage = CalculatePenetration(_damage, _penetration - _currentPenetration);
                     EEntityPartType parts = damageable.ApplyDamage(damage, hit, transform);
 
                     if (damageable is EntityHealth health && health.TryGetComponent(out Entity entity))
@@ -73,21 +80,43 @@ namespace Hashira.Projectile
                     gameObject.Pop(_spakleEffect, hit.point + hit.normal * 0.1f, Quaternion.LookRotation(Vector3.back, hit.normal));
                 }
 
-                //Die
-                Die();
+                // Penetration
+                if (hit.transform.TryGetComponent(out IPenetrable penetrable))
+                {
+                    _currentPenetration -= penetrable.Resistivity;
+                    if (_currentPenetration > 0)
+                    {
+
+                        _penetratedColliderList.Add(hit.collider);
+                    }
+                    else
+                        Die();
+                }
+                else
+                    Die();
+
             }
             else
                 transform.position += movement;
         }
 
-        public void Init(LayerMask whatIsTarget, Vector3 direction, float speed, int damage)
+        public int CalculatePenetration(float damage, int penetratedCount)
+        {
+            if (penetratedCount == 0) return Mathf.CeilToInt(damage);
+            return CalculatePenetration(damage * 0.8f, penetratedCount - 1);
+        }
+
+        public void Init(LayerMask whatIsTarget, Vector3 direction, float speed, int damage, int penetration)
         {
             _damage = damage;
             _speed = speed;
+            _penetration = penetration;
+            _currentPenetration = penetration;
             _whatIsTarget = whatIsTarget;
             transform.right = direction;
             _spriteRenderer.enabled = true;
             _trailRenderer.enabled = true;
+            _penetratedColliderList = new List<Collider2D>();
         }
 
         public override void Die()
