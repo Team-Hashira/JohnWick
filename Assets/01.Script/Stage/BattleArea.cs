3b5bfcca1f1e3dd9d793fc32f1c165251985bf50
Crogen0407 <mymcoding0407@gmail.com>
2025-02-10 01:29:02 +0900

    feat : BattleArea size

--- Assets/01.Script/Stage/BattleArea.cs
+++ Assets/01.Script/Stage/BattleArea.cs
@@ -14,19 +14,52 @@ namespace Hashira.Stage
 		[SerializeField] private UnityEvent BattleEndEvent;
 		[SerializeField] private UnityEvent PlayerEnterEvent;
 		[SerializeField] private List<EntityHealth> _entityList;
+
+		[SerializeField] private Vector2 _size = new Vector2(17.78f, 10.05f);
+
 		private int _enemyCount = 0;
 
 		public event Action ClearEvent;
 
 		private CinemachineCamera _cam;
-
 		private CameraManager _cameraManager;
+		private PolygonCollider2D _polygonCollider;
 
 		private void Awake()
 		{
 			_cam = GetComponentInChildren<CinemachineCamera>();
 			_cam.Priority = -1;
 		}
+
+#if UNITY_EDITOR
+		private void OnValidate()
+		{
+			_polygonCollider ??= GetComponent<PolygonCollider2D>();
+
+			Vector2[] sizes = new Vector2[4];
+
+			sizes[0] = _size;
+			sizes[1] = new Vector2(-_size.x, _size.y);
+			sizes[2] = -_size;
+			sizes[3] = new Vector2(_size.x, -_size.y);
+
+			_polygonCollider.SetPath(0, sizes);
+
+			Transform r = transform.Find("R");
+			Transform l = transform.Find("L");
+
+			var colls = GetComponentsInChildren<BoxCollider2D>();
+			foreach (var coll in colls)
+				coll.size = new Vector2(1f, _polygonCollider.GetPath(0)[0].y * 2);
+
+			if (r != null)
+				r.localPosition = new Vector3(_polygonCollider.GetPath(0)[0].x + 0.5f, 0, 0);
+			if (l != null)
+				l.localPosition = new Vector3(_polygonCollider.GetPath(0)[1].x - 0.5f, 0, 0);
+
+			EditorUtility.SetDirty(_polygonCollider);
+		}
+#endif
 
 		private void Start()
 		{
@@ -37,24 +70,7 @@ namespace Hashira.Stage
 			foreach (var entity in _entityList)
 				entity.OnDieEvent += HandleCounting;
 		}
-#if UNITY_EDITOR
-		private void OnDrawGizmosSelected()
-		{
-			PolygonCollider2D polygonCollider = GetComponent<PolygonCollider2D>();
 
-			Transform r = transform.Find("R");
-			Transform l = transform.Find("L");
-
-			var colls = GetComponentsInChildren<BoxCollider2D>();
-			foreach (var coll in colls)
-				coll.size = new Vector2(1f, polygonCollider.GetPath(0)[0].y*2);
-
-			if (r != null)
-				r.localPosition = new Vector3(polygonCollider.GetPath(0)[0].x + 0.5f, 0, 0);
-			if (l != null)
-				l.localPosition = new Vector3(polygonCollider.GetPath(0)[1].x - 0.5f, 0, 0);
-		}
-#endif
 		public void StartBattle()
 		{
 			BattleStartEvent?.Invoke();
