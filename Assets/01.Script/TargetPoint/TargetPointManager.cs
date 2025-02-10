07b9791526310992a2c8e911c8da8de2cc908247
Crogen0407 <mymcoding0407@gmail.com>
2025-02-10 18:44:18 +0900

    feat : TargetPoint System

---
+++ Assets/01.Script/TargetPoint/TargetPointManager.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using UnityEngine;
+using Crogen.CrogenPooling;
+using Hashira.TargetPoint.UI;
+
+namespace Hashira.TargetPoint
+{
+	public class TargetPointManager : MonoSingleton<TargetPointManager>
+	{
+		[SerializeField] private Transform _parentCanvas;
+		[SerializeField] private OtherPoolType _targetPointContentPoolType = OtherPoolType.TargetPointContent;
+		private Dictionary<Transform, TargetPointContent> _targetPointContentDictionary = new Dictionary<Transform, TargetPointContent>();
+
+		public void ShowTargetPoint(Transform trm, Color color)
+		{
+			if (_targetPointContentDictionary.ContainsKey(trm)) return;
+
+			TargetPointContent targetPoint = gameObject.Pop(_targetPointContentPoolType, _parentCanvas) as TargetPointContent;
+			if (targetPoint == null) return;
+
+			_targetPointContentDictionary.Add(trm, targetPoint);
+			targetPoint.targetTrasform = trm;
+			targetPoint.ImageColor = color;
+		}
+
+		public void CloseTargetPoint(Transform trm)
+		{
+			if (!_targetPointContentDictionary.ContainsKey(trm)) return;
+
+			TargetPointContent targetPoint = _targetPointContentDictionary[trm];
+			if (targetPoint == null) return;
+
+			_targetPointContentDictionary.Remove(trm);
+			targetPoint.Push();
+		}
+	}
+}
