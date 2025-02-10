0da07a890b2b2a01d5a6de8bddcc0997316b8654
Crogen0407 <mymcoding0407@gmail.com>
2025-02-10 19:43:10 +0900

    feat : TargetPoint View

--- Assets/01.Script/TargetPoint/TargetPointContent.cs
+++ Assets/01.Script/TargetPoint/TargetPointContent.cs
@@ -27,9 +27,11 @@ namespace Hashira.TargetPoint.UI
 
 		[SerializeField] private Image _image;
 		[SerializeField] private TextMeshProUGUI _distanceText;
+		private RectTransform _rectTransform;
 
 		private void Awake()
 		{
+			_rectTransform = transform as RectTransform;
 			_playerCameraTrm = Camera.main.transform;
 			_playerTrm = GameManager.Instance.Player.transform;
 		}
@@ -42,23 +44,34 @@ namespace Hashira.TargetPoint.UI
 
 			float minY = _image.GetPixelAdjustedRect().height / 2;
 			float maxY = Screen.height - minY;
-
-			Vector2 pos = Camera.main.WorldToScreenPoint(targetTrasform.position);
+			Vector2 originPos = Camera.main.WorldToScreenPoint(targetTrasform.position);
+			Vector2 pos = originPos;
 
 			if (Vector3.Dot((targetTrasform.position - _playerCameraTrm.position), _playerCameraTrm.forward) < 0)
 			{
 				if (pos.x < Screen.width / 2)
-				{
 					pos.x = maxX;
-				}
 				else
-				{
 					pos.x = minX;
-				}
+			}
+
+
+			if(pos.x < minX || pos.x > maxX || pos.y < minY || pos.y > maxY)
+			{
+				_image.enabled = true;
+				_distanceText.enabled = true;
+			}
+			else
+			{
+				_image.enabled = false;
+				_distanceText.enabled = false;
 			}
 
 			pos.x = Mathf.Clamp(pos.x, minX, maxX);
 			pos.y = Mathf.Clamp(pos.y, minY + 37.4f, maxY);
+
+			Vector2 dir = (originPos - pos).normalized;
+			_rectTransform.rotation = Quaternion.Euler(0, 0, Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90);
 
 			_distanceText.text = $"{(int)Vector3.Distance(targetTrasform.position, _playerTrm.position)}m";
 
