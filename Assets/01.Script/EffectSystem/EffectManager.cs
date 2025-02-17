8aec7f8ca533fcc02d0a7f44d641c1555f544271
Crogen0407 <mymcoding0407@gmail.com>
2025-02-17 14:39:34 +0900

    fix : effect system

--- Assets/01.Script/EffectSystem/EffectManager.cs
+++ Assets/01.Script/EffectSystem/EffectManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Hashira.EffectSystem.Effects;
 using Hashira.Entities;
 using UnityEngine;
 
@@ -9,68 +10,79 @@ namespace Hashira.EffectSystem
     public class EffectManager : MonoSingleton<EffectManager>
     {
         [SerializeField] private List<EffectUIDataSO> _effectUIDataSOList = new List<EffectUIDataSO>();
-        private readonly List<Effect> _effectList = new List<Effect>();
+        private readonly Dictionary<Entity, List<Effect>> _effectDict = new Dictionary<Entity, List<Effect>>();
 
         public event Action<Effect> EffectAddedEvent;
         public event Action<Effect> EffectRemovedEvent;
 
-        public void AddEffect<T>(EntityEffector baseEffector, int level, float duration) where T : Effect, new()
+        public void AddEffect<T>(Entity entity, int level, float duration) where T : Effect, new()
         {
             T effect = new T()
             {
                 name = typeof(T).Name,
                 duration = duration,
                 level = level,
-                effectUIDataSO = _effectUIDataSOList.FirstOrDefault(x=>x.name == typeof(T).Name),
-                baseEffector = baseEffector
+                effectUIDataSO = _effectUIDataSOList.FirstOrDefault(x => x.name == typeof(T).Name),
+                entity = entity,
+				entityStat = entity.GetEntityComponent<EntityStat>()
             };
-            
-            _effectList.Add(effect);
+
+            _effectDict.TryAdd(entity, new List<Effect>());
+
+            _effectDict[entity].Add(effect);
             
             EffectAddedEvent?.Invoke(effect);
-        }
+			effect.Enable();
+		}
 
-        public void RemoveEffect<T>(EntityEffector baseEffector) where T : Effect
+		public void RemoveEffect<T>(Entity entity) where T : Effect
         {
-            Effect removeEffect = _effectList.FirstOrDefault(x=>x.name==typeof(T).Name);
+            Effect removeEffect = _effectDict[entity].FirstOrDefault(x=>x.name==typeof(T).Name);
 
             if (removeEffect != null)
             {
-                _effectList.Remove(removeEffect);
+                _effectDict[entity].Remove(removeEffect);
                 
                 EffectRemovedEvent?.Invoke(removeEffect);
             }
             else
                 Debug.Log($"Effect {typeof(T).Name} was not found");
-        }
-        
-        public void RemoveEffect(EntityEffector baseEffector, Type effectType)
+
+			removeEffect.Disable();
+		}
+
+		public void RemoveEffect(Entity entity, Type effectType)
         {
-            Effect removeEffect = _effectList.FirstOrDefault(x=>x.name==effectType.Name);
+            Effect removeEffect = _effectDict[entity].FirstOrDefault(x=>x.name==effectType.Name);
 
             if (removeEffect != null)
             {
-                _effectList.Remove(removeEffect);
+                _effectDict[entity].Remove(removeEffect);
                 
                 EffectRemovedEvent?.Invoke(removeEffect);
             }
             else
                 Debug.Log($"Effect {effectType.Name} was not found");
-        }
-        
-        private void Update()
+
+			removeEffect.Disable();
+		}
+
+		private void Update()
         {
-            for (int i = 0; i < _effectList.Count; i++)
+            foreach (var effectList in _effectDict)
             {
-                var effect = _effectList[i];
-                
-                effect.Update();
-                effect.currentTime += Time.deltaTime;
-                if (effect.currentTime >= effect.duration)
-                {
-                    RemoveEffect(effect.baseEffector, effect.GetType());
-                }
-            }
+				for (int i = 0; i < effectList.Value.Count; i++)
+				{
+					var effect = effectList.Value[i];
+
+					effect.Update();
+					effect.currentTime += Time.deltaTime;
+					if (effect.currentTime >= effect.duration)
+					{
+						RemoveEffect(effect.entity, effect.GetType());
+					}
+				}
+			}
         }
     }
 }
