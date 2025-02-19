ca3bc2d168311a4159e844e0601d0fcd002999c9
Crogen0407 <mymcoding0407@gmail.com>
2025-02-19 14:57:35 +0900

    feat : Inventory

--- Assets/01.Script/UI/StatusWindow/WeaponSlot/Inventory.cs
+++ Assets/01.Script/UI/StatusWindow/WeaponSlot/Inventory.cs
@@ -1,19 +1,39 @@
+using Hashira.Items.PartsSystem;
+using System;
 using UnityEngine;
 
-namespace Hashira
+namespace Hashira.UI.StatusWindow
 {
     public class Inventory : MonoBehaviour
     {
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
+        private PartSlot[] slots;
+        private WeaponParts[] weaponParts = new WeaponParts[20];
+
+        private void Awake()
         {
-        
+            slots = GetComponentsInChildren<PartSlot>();
+            for (int i = 0; i < slots.Length; i++)
+            {
+                slots[i].isAllType = true;
+                slots[i].Init(null, null);
+                slots[i].OnChangedPartsEvent += HandleSlotChanged;
+            }
         }
 
-        // Update is called once per frame
-        void Update()
+        private void OnDestroy()
         {
-        
+            for (int i = 0; i < slots.Length; i++)
+                slots[i].OnChangedPartsEvent -= HandleSlotChanged;
+        }
+
+        private void HandleSlotChanged()
+        {
+            for (int i = 0;i < weaponParts.Length; i++)
+            {
+                weaponParts[i] = slots[i].BasePart;
+                Debug.Log(weaponParts[i]);
+            }
+            Debug.Log("----------------------------------");
         }
     }
 }
