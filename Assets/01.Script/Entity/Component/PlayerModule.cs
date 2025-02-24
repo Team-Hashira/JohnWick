c141ad34a89a850c362777d613e2b48e4ab0819e
dongdong5899 <dong012673@gmail.com>
2025-02-25 05:06:09 +0900

    feat: Module Component

--- Assets/01.Script/Entity/Component/PlayerModule.cs
+++ Assets/01.Script/Entity/Component/PlayerModule.cs
@@ -1,11 +1,10 @@
-using Hashira.Entities;
 using Hashira.Items;
 using Hashira.Players;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-namespace Hashira
+namespace Hashira.Entities.Components
 {
     public class PlayerModule : MonoBehaviour, IEntityComponent
     {
@@ -16,19 +15,32 @@ namespace Hashira
         public void Initialize(Entity entity)
         {
             _player = entity as Player;
+            ListModules = new List<ModuleItem>();
+        }
+
+        private void Update()
+        {
+            for (int i = 0; i < ListModules.Count; i++)
+            {
+                ListModules[i].ItemUpdate();
+            }
         }
 
         public void AddModule(ModuleItem moduleItem)
-        { 
+        {
             ListModules.Add(moduleItem);
+            moduleItem.Equip(_player);
         }
 
         public void RemoveModule<T>(T moduleItem) where T : ModuleItem
         {
-            ModuleItem removeTargetModule 
+            ModuleItem removeTargetModule
                 = ListModules.FirstOrDefault(module => module.GetType() == typeof(T));
             if (removeTargetModule != default)
+            {
                 ListModules.Remove(removeTargetModule);
+                moduleItem.UnEquip();
+            }
         }
     }
 }
