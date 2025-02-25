7104c6121dd2084374e0b51af81f3a059835a33e
dongdong5899 <dong012673@gmail.com>
2025-02-25 17:40:28 +0900

    fix: Blast

--- Assets/01.Script/Item/Module/BlastBulletModule.cs
+++ Assets/01.Script/Item/Module/BlastBulletModule.cs
@@ -1,26 +1,28 @@
+using Crogen.CrogenPooling;
 using Hashira.Players;
 using Hashira.Projectiles;
+using UnityEngine;
 
 namespace Hashira.Items.Modules
 {
-    public class BlastBulletModule : Module
+    public class BlastBulletModule : Module, IProjectileModifier
     {
-        private BlastBulletModifier _modifier;
         private bool _isCanBlastBullet = true;
-        float _delay = 8f
-            ;
+        float _delay = 8f;
+        public int doubleAttackDamage = 100;
+        public Projectile _projectile;
+
         public override void Equip(Player player)
         {
             base.Equip(player);
-            _modifier = new BlastBulletModifier();
             player.Attacker.OnProjectileCreateEvent += HandleProjectileCreateEvent;
-            player.Attacker.AddProjectileModifiers(_modifier);
+            player.Attacker.AddProjectileModifiers(this);
         }
 
         private void HandleProjectileCreateEvent()
         {
             _player.Attacker.OnProjectileCreateEvent -= HandleProjectileCreateEvent;
-            _player.Attacker.RemoveProjectileModifiers(_modifier);
+            _player.Attacker.RemoveProjectileModifiers(this);
             CooldownUtillity.StartCooldown("BlastBullet");
             _isCanBlastBullet = false;
         }
@@ -32,7 +34,7 @@ namespace Hashira.Items.Modules
             {
                 _isCanBlastBullet = true;
                 _player.Attacker.OnProjectileCreateEvent += HandleProjectileCreateEvent;
-                _player.Attacker.AddProjectileModifiers(_modifier);
+                _player.Attacker.AddProjectileModifiers(this);
             }
         }
 
@@ -41,6 +43,27 @@ namespace Hashira.Items.Modules
             base.UnEquip();
             if (CooldownUtillity.CheckCooldown("AimingBullet", _delay))
                 HandleProjectileCreateEvent();
+        }
+
+        public void OnProjectileCreate(Projectile projectile)
+        {
+            for (int i = -1; i <= 1; i++)
+            {
+                var blastBulletModuleSplinter = PopCore.Pop(ProjectilePoolType.BlastBulletModuleSplinter, _projectile.transform.position + Vector3.up * 0.25f, Quaternion.Euler(0, 0, Random.Range(0, 360f))) as BlastBulletModuleSplinter;
+                Vector2 dir = new Vector2(i * 0.55f, 1).normalized * 300;
+                dir += Random.insideUnitCircle.normalized * 10f;
+                blastBulletModuleSplinter.Init(dir, doubleAttackDamage);
+            }
+        }
+
+        public void OnProjectileUpdate()
+        {
+
+        }
+
+        public void OnProjectileHit(RaycastHit2D hit, IDamageable damageable)
+        {
+            doubleAttackDamage = (int)(_projectile.Damage * 0.3f);
         }
     }
 }
