522a5b44d09dd94eb9787389dd8bc2b810785bcc
SundG0162 <123521471+SundG0162@users.noreply.github.com>
2025-03-06 08:26:56 +0900

    feat : MathEx.Bezier

--- Assets/01.Script/Core/Extension/ExtensionMethods.cs
+++ Assets/01.Script/Core/Extension/ExtensionMethods.cs
@@ -3,13 +3,25 @@ using UnityEngine;
 
 public static class ExtensionMethods
 {
-    
+
 }
 
 public static class MathEx
 {
-    public static Vector2 Bezier(Vector2 pos1, Vector2 pos2)
+    public static Vector2 Bezier(float t, params Vector2[] positions)
     {
-        return Vector2.zero;
+        if (positions.Length == 0) 
+            return Vector2.zero;
+        if (positions.Length == 1) 
+            return positions[0];
+
+        Vector2[] nextPositions = new Vector2[positions.Length - 1];
+
+        for (int i = 0; i < nextPositions.Length; i++)
+        {
+            nextPositions[i] = Vector2.Lerp(positions[i], positions[i + 1], t);
+        }
+
+        return Bezier(t, nextPositions);
     }
 }
