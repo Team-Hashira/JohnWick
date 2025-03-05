de0563b40484a670f735a9f1ad8e6e96df8398c9
dongdong5899 <dong012673@gmail.com>
2025-03-05 18:37:55 +0900

    feat: VFX Effect

--- Assets/01.Script/Projectile/Bullet.cs
+++ Assets/01.Script/Projectile/Bullet.cs
@@ -31,7 +31,7 @@ namespace Hashira.Projectiles
 
                 if (damageable is EntityHealth health && health.TryGetComponent(out Entity entity))
                 {
-                    gameObject.Pop(EffectPoolType.BulletHitEffect, hit.point + hit.normal * 0.1f, Quaternion.LookRotation(Vector3.back, hit.normal));
+                    gameObject.Pop(EffectPoolType.BulletHitEffect, hit.point + hit.normal * 0.1f, Quaternion.LookRotation(Vector3.back, -hit.normal));
                     gameObject.Pop(EffectPoolType.HitBlood, hit.point + hit.normal * 0.1f, Quaternion.LookRotation(Vector3.back, hit.normal));
                     // TODO 데미지 입혀야 함
                 }
